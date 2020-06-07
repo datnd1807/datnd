dceb2d4937d3021bf5bdd0b957a5f433d4df117b
DATDUY <datnd1807@gmail.com>
2020-06-07 09:48:41 +0700

    Detect Face
    Detect Face

---
+++ EAWApp/EAWApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace EAWApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.lblDevice = new System.Windows.Forms.Label();
+            this.cbbDevice = new System.Windows.Forms.ComboBox();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.btnAddPerson = new System.Windows.Forms.Button();
+            this.txtPerName = new System.Windows.Forms.TextBox();
+            this.lblPerName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCapture
+            // 
+            this.picCapture.BackColor = System.Drawing.SystemColors.Desktop;
+            this.picCapture.Location = new System.Drawing.Point(13, 13);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(450, 450);
+            this.picCapture.TabIndex = 0;
+            this.picCapture.TabStop = false;
+            // 
+            // lblDevice
+            // 
+            this.lblDevice.AutoSize = true;
+            this.lblDevice.Location = new System.Drawing.Point(471, 16);
+            this.lblDevice.Name = "lblDevice";
+            this.lblDevice.Size = new System.Drawing.Size(59, 17);
+            this.lblDevice.TabIndex = 1;
+            this.lblDevice.Text = "Device: ";
+            // 
+            // cbbDevice
+            // 
+            this.cbbDevice.FormattingEnabled = true;
+            this.cbbDevice.Location = new System.Drawing.Point(536, 13);
+            this.cbbDevice.Name = "cbbDevice";
+            this.cbbDevice.Size = new System.Drawing.Size(252, 24);
+            this.cbbDevice.TabIndex = 2;
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(536, 44);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(252, 23);
+            this.btnDetect.TabIndex = 3;
+            this.btnDetect.Text = "Detect Face";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // btnAddPerson
+            // 
+            this.btnAddPerson.Location = new System.Drawing.Point(536, 102);
+            this.btnAddPerson.Name = "btnAddPerson";
+            this.btnAddPerson.Size = new System.Drawing.Size(252, 23);
+            this.btnAddPerson.TabIndex = 4;
+            this.btnAddPerson.Text = "Add Person";
+            this.btnAddPerson.UseVisualStyleBackColor = true;
+            // 
+            // txtPerName
+            // 
+            this.txtPerName.Location = new System.Drawing.Point(536, 74);
+            this.txtPerName.Name = "txtPerName";
+            this.txtPerName.Size = new System.Drawing.Size(252, 22);
+            this.txtPerName.TabIndex = 5;
+            // 
+            // lblPerName
+            // 
+            this.lblPerName.AutoSize = true;
+            this.lblPerName.Location = new System.Drawing.Point(471, 77);
+            this.lblPerName.Name = "lblPerName";
+            this.lblPerName.Size = new System.Drawing.Size(49, 17);
+            this.lblPerName.TabIndex = 6;
+            this.lblPerName.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.lblPerName);
+            this.Controls.Add(this.txtPerName);
+            this.Controls.Add(this.btnAddPerson);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.cbbDevice);
+            this.Controls.Add(this.lblDevice);
+            this.Controls.Add(this.picCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.Label lblDevice;
+        private System.Windows.Forms.ComboBox cbbDevice;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.Button btnAddPerson;
+        private System.Windows.Forms.TextBox txtPerName;
+        private System.Windows.Forms.Label lblPerName;
+    }
+}
+
---
+++ EAWApp/EAWApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAWApp
+{
+    public partial class Form1 : Form
+    {
+        private HaarCascade faceDetect;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainningImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLabels, t;
+        string name, names = null;
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX,0.6d,0.6d);
+        public Form1()
+        {
+            InitializeComponent();
+            faceDetect = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+                // the first label before, will be the number of faces saved
+                NumLabels = Convert.ToInt16(Labels[0]);
+                Count = NumLabels;
+                string FacesLoad;
+                for(int i = 1; i< NumLabels + 1; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    trainningImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nothing in the Database");
+            }
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            camera = new Capture(1);
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetect,1.2,10,Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20,20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if(trainningImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriteria = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainningImages.ToArray(), labels.ToArray(), 1500, ref termCriteria);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2),
+                        new Bgr(Color.Red));
+                }
+             
+                Users.Add("");
+            }
+            picCapture.Image = Frame.Bitmap;
+            name = "";
+            Users.Clear();
+        }
+    }
+}
